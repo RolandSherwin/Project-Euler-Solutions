04901b9f98962984c94c9e2274d5ed667e65f7ec
RolandSherwin <RolandSherwin@protonmail.com>
2020-12-29 11:57:01 +0530

    added preprocess_data

--- Python_Solutions/utils/misc.py
+++ Python_Solutions/utils/misc.py
@@ -1,4 +1,5 @@
 import time
+import re
 
 
 def timeit(method):
@@ -10,3 +11,10 @@ def timeit(method):
         print(f"{method.__name__} took: {elapsed} ms")
         return result
     return timed
+
+
+def preprocess_data(number_as_string):
+    # Removes special characters and put them in list. You have to process after
+    # this
+    arr = re.findall(r"[\w']+", number_as_string)
+    return arr
