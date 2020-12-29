8e486f9ba5215c238693d63ea6e0b8661a4e520d
RolandSherwin <RolandSherwin@protonmail.com>
2020-12-29 11:29:25 +0530

    Added number of factors

--- Python_Solutions/utils/primes.py
+++ Python_Solutions/utils/primes.py
@@ -1,3 +1,6 @@
+from collections import Counter
+
+
 def is_prime(n: int) -> bool:
     """Primality test using 6k+-1 optimization."""
     if n <= 3:
@@ -36,5 +39,20 @@ def prime_factors(number):
     return primes
 
 
+def number_of_factors(number):
+    """ You can find the number of factors/divisors of a given number by first
+    finding the prime factors. ie N = p_1^a * p_2^b * p_3^b;
+    Then the factors = (a+1)(b+1)(c+1)... since every factor can occur zero upto
+    "a" (or b,c..) times
+    Eg: 12 = 2^2 * 3^1 so number of factors = 3*2 = 6; 
+    which are 1,2,3,4,6,12
+    """
+    prime_factors_count = dict(Counter(prime_factors(number)))
+    factors_count = 1
+    for key, value in prime_factors_count.items():
+        factors_count *= value+1
+    return factors_count
+
+
 if __name__ == "__main__":
-    pass
+    print(number_of_factors(76576500))
